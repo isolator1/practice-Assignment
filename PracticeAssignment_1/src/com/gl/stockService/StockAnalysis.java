6047d41681c5293705d8b6b27d0fbd602f886f3e
ramakrishnan <102976005+isolator1@users.noreply.github.com>
2022-07-17 18:19:35 +0530

    Add files via upload

--- PracticeAssignment_1/src/com/gl/stockService/StockAnalysis.java
+++ PracticeAssignment_1/src/com/gl/stockService/StockAnalysis.java
@@ -1,6 +1,10 @@
 package com.gl.stockService;
 
+import java.util.Arrays;
+
 import java.util.Scanner;
+
+
 import com.gl.sorting.MergeSortAsc;
 import com.gl.sorting.MergeSortDsc;
 import com.gl.searching.BinarySearch;
@@ -10,20 +14,19 @@ public class StockAnalysis implements IStockAnalysisService {
 
 	@Override
 	public double[] stockPriceAscendingOrder(double[] stockPriceArray) {
-		MergeSortAsc msasc = new MergeSortAsc();
-		msasc.mergeSortAsc(stockPriceArray, 0, stockPriceArray.length - 1);
+		MergeSortAsc.mergeSortAsc(stockPriceArray, 0, stockPriceArray.length - 1);
 		return stockPriceArray;
 	}
 
 	@Override
 	public double[] stockPriceDescendingOrder(double[] stockPriceArray) {
-		MergeSortDsc msdsc = new MergeSortDsc();
-		msdsc.mergeSortDsc(stockPriceArray, 0, stockPriceArray.length - 1);
+		MergeSortDsc.mergeSortDsc(stockPriceArray, 0, stockPriceArray.length - 1);
 		return stockPriceArray;
 	}
 
 	@Override
 	public void stockPriceComparisionRise(Boolean[] priceStatus) {
+		
 		int countOfStatusTrue = 0;
 		for (int i = 0; i < priceStatus.length; i++) {
 			if (priceStatus[i] == true) {
@@ -46,17 +49,15 @@ public class StockAnalysis implements IStockAnalysisService {
 
 	@Override
 	public void stockPriceAvailability(double[] stockPriceArray) {
-		BinarySearch bs = new BinarySearch();
+		System.out.println(Arrays.toString(stockPriceArray));
 		System.out.println("Enter key value to be found in the list");
 		double keyValue = sc.nextDouble();
-		double priceKeyValue = bs.binarySearch(stockPriceArray, 0, stockPriceArray.length - 1, keyValue);
-//		System.out.println(priceKeyValue);
-//		System.out.println(stockPriceArray.length);
-//		System.out.println(Arrays.toString(stockPriceArray));
+		MergeSortAsc.mergeSortAsc(stockPriceArray, 0, stockPriceArray.length - 1);
+		double priceKeyValue = BinarySearch.binarySearch(stockPriceArray, 0, stockPriceArray.length - 1, keyValue);
 		if (priceKeyValue > -1) {
 			System.out.println("Stock of Value " + keyValue + " is present");
 		} else {
-			System.out.println("Stock of Value " + keyValue + " is not present");
+			System.out.println("Stock of Value " + keyValue + " is absent");
 		}
 
 	}
